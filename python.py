c7da71626ed618b263463b6add801738e70fbf7b
LegendAqdas007 <aqdaskhn786@gmail.com>
2024-11-14 16:11:34 +0000

    first lesson

--- python.py
+++ python.py
@@ -1,1 +1,15 @@
 
+def printhello():
+    # function definition
+    for i in range(1,10):
+        print("hello")
+
+# print("ghhdtyh")
+# printhello()#call
+
+x=34
+y=34.5
+a={1:"anand",2:"anant",3:"anushka",4:"aqdas"}
+b={"a":"anand","b":"anant","c":"anushka","d":"aqdas"}
+val=True
+print(b.get("a"))
